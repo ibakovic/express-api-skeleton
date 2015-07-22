2d278914933d8a0ae763a1d3dd9af033607a5b7c
ivan <ivbakovi@fesb.hr>
2015-07-22 11:07:16 +0200

    noSrc

--- app.js
+++ app.js
@@ -1,111 +1,3 @@
-/*var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-//MongoDB
-var mongoose = require('mongoose');
-require('./models/posts');
-require('./models/auths');
-require('./models/token');
-mongoose.connect('mongodb://localhost/movies');
-
-/*
-var pas = mongoose.model('Dog', {dog:String});
-
-var d = new pas({dog: 'Neki pas'});*/
-/*d.save(function (err) { 
-    if(err) console.log('not working');
-});
-*/
-//pas.find({ dog:'Neki pas' }).remove().exec();
-/*DELETE FUNKCIJA
-var video = mongoose.model('Post');
-video.find({title: 'u'}).remove().exec();
-
-
-/*
-var Cat = mongoose.model('Cat', { name: String });
-var videos = mongoose.model('Video', {vid_name: String});
-var dog = mongoose.model('Dogie', {dog:String});
-
-var d = new dog({dog: 'My dog'});
-d.save(function (err) { 
-    if(err) console.log('not working');
-});
-
-var vid = new videos({vid_name: '1st video'});
-vid.save(function (err) { 
-    if(err) console.log('not working');
-});
-
-var kitty = new Cat({ name: 'Zildjian' });
-kitty.save(function (err) {
-  if (err) // ...
-  console.log('meow');
-});
-*//*
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
-
-app.use(routes);
-// include error handlers
-errors(app);
-
-module.exports = app;
-*/
-
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -171,5 +63,8 @@ app.use(function(err, req, res, next) {
     });
 });
 
+app.use(routes);
+// include error handlers
+errors(app);
 
 module.exports = app;
--- src/models/auths.js
+++
@@ -1,8 +0,0 @@
-var mongoose = require('mongoose');
-
-var AuthSchema = new mongoose.Schema({
-  username: String,
-  password: String
-});
-
-mongoose.model('Auth', AuthSchema);
