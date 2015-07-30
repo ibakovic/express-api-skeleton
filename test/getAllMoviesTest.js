01272417ef7046900ea9fff36de4b4462269f796
ivan <ivbakovi@fesb.hr>
2015-07-30 10:51:06 +0200

    second test

--- test/getAllMoviesTest.js
+++ test/getAllMoviesTest.js
@@ -1,6 +1,5 @@
 var request = require('supertest');
 var app = require('../app');
-var errors = require('../views/errors');
 console.log("app: " + app.get('/getAllMovies'));
 
 
@@ -12,7 +11,7 @@ describe('../controllers/utils/getAllMovies.js', function () {
       //.expect('Content-Type', /json/)
       .expect(200)
       .end(function(err, res){
-        if (err) throw errors();
+        if (err) throw err;
         done();
     });
   });
