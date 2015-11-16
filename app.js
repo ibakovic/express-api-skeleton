3a6d25fffd4fd1785787257656d7e5bd253fc0a9
ibakovic <ivbakovi@fesb.hr>
2015-11-16 13:08:26 +0100

    App.js and bin/www files mongodb friendly
    -connection string to openshift mongodb added to app.js

--- app.js
+++ app.js
@@ -11,7 +11,23 @@ var mongoose = require('mongoose');
 require('./models/posts');
 require('./models/auths');
 require('./models/token');
-mongoose.connect('mongodb://localhost/movies');
+//mongoose.connect('mongodb://localhost/movies');
+
+var connection_string = 'localhost/movies';
+// if OPENSHIFT env variables are present, use the available connection info:
+if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+  connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
+}
+
+mongoose.connect('mongodb://'+connection_string);
+//load the Client interface
+//var MongoClient = require('mongodb').MongoClient;
+// the client db connection scope is wrapped in a callback:
+//MongoClient.connect('mongodb://'+connection_string, ['movies']);
 
 var routes = require('./index');
 
