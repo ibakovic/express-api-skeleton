92233e702ae6df395211db050d690604439b339a
ibakovic <ivbakovi@fesb.hr>
2015-11-24 09:13:15 +0100

    small app changes

--- app.js
+++ app.js
@@ -31,12 +31,17 @@ var connectionStr = 'localhost/movies';
 
 var connection_string = 'localhost/movies';
 if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-  connectionStr = util.format('mongodb://%s:%s@%s:%d/%s',
+  /*connectionStr = util.format('mongodb://%s:%s@%s:%d/%s',
     process.env.OPENSHIFT_MONGODB_DB_USERNAME,
     process.env.OPENSHIFT_MONGODB_DB_PASSWORD,
     process.env.OPENSHIFT_MONGODB_DB_HOST,
     process.env.OPENSHIFT_MONGODB_DB_PORT,
-    process.env.OPENSHIFT_APP_NAME);
+    process.env.OPENSHIFT_APP_NAME);*/
+connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+  process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+  process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+  process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+  process.env.OPENSHIFT_APP_NAME;
 }
 
 mongoose.connect('mongodb://'+connection_string);
